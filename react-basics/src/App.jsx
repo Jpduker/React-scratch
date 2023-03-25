c3db1326d7cf15fc17c21865ae7c86e352571faa
Jpduker <jayaprakashnis619@gmail.com>
2023-03-25 18:16:51 +0530

    added basic functional component

---
+++ react-basics/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Employee from './Employee'
+import './App.css'
+
+function App() {
+
+  return (
+    <Employee/>
+  )
+}
+
+export default App
---
+++ react-basics/src/Employee.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Employee(props) {
+  return (
+    <div>
+        <h1>name </h1>
+        <h1>salary</h1>
+
+    </div>
+  )
+}
+export default Employee;
